b30fc680167150771c9c33f5eab42dd3ae900d26
aidasdetail <122400708+aidasdetail@users.noreply.github.com>
2023-01-11 14:16:40 +0100

    d

--- js/nav.js
+++ js/nav.js
@@ -2,17 +2,17 @@
    let nav = document.querySelector('.navbar');
    
    nav.innerHTML = `
-   <div class="nav">
-    <img src="img/Logo.png" class="brand-logo" alt="">
-    <div class="nav-items">
-        <div class="search">
-            <input type="text" class="search-box" placeholder="search brand, product">
-            <button class="search-btn">search</button>
+    <div class="nav">
+        <img src="img/Logo.png" class="brand-logo" alt="">
+        <div class="nav-items">
+            <div class="search">
+                <input type="text" class="search-box" placeholder="search brand, product">
+                <button class="search-btn">search</button>
+            </div>
+            <a href="#"><img src="img/1.png" alt=""></a>
+            <a href="#"><img src="img/3.png" alt=""></a>
         </div>
-        <a href="#"><img src="img/1.png" alt=""></a>
-        <a href="#"><img src="img/3.png" alt=""></a>
     </div>
- </div>
     
     <ul class="links-container">
         <li class="link-item"><a href="#" class="link">home</a></li>
